37ae537bc6e3375bb36c56031ba01440c8f45b29
YellowTech <oliver.fcst@hotmail.com>
2022-04-16 22:00:03 +0200

    Added Travel Time display

--- src/Village.tsx
+++ src/Village.tsx
@@ -39,6 +39,19 @@ function Village (props: { name: string, x: number, y: number, xTarget: number, 
     3000 // 12 Trebuchet
   ]
 
+  function secondsToDhms(seconds: number) {
+      var d = Math.floor(seconds / (3600 * 24));
+      var h = Math.floor((seconds % (3600 * 24)) / 3600);
+      var m = Math.floor((seconds % 3600) / 60);
+      var s = Math.floor(seconds % 60);
+
+      var dDisplay = d > 0 ? d + "d" : "";
+      var hDisplay = h > 0 ? h + "h" : "";
+      var mDisplay = m > 0 ? m + "m" : "";
+      var sDisplay = s > 0 ? s + "s" : "";
+      return dDisplay + hDisplay + mDisplay + sDisplay;
+  }
+
   return (
     <div>
       <Card>
@@ -89,8 +102,11 @@ function Village (props: { name: string, x: number, y: number, xTarget: number, 
                 <Col sm={3}>
                   <p>Distance: {dist.toFixed(2)}</p>
                 </Col>
+                <Col sm={3}>
+                  <p className="text-center">Travel Time: {secondsToDhms((map[troop] as number * dist))}</p>
+                </Col>
                 <Col sm={6}>
-                  <p>Departure Time: {new Date(props.attackDate - (map[troop] as number * dist * 1000)).toLocaleString()}</p>
+                  <p className="text-right">Departure Time: {new Date(props.attackDate - (map[troop] as number * dist * 1000)).toLocaleString()}</p>
                 </Col>
               </Row>
             </Card.Body>
